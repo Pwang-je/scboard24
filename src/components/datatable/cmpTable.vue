3795681a3aa639504db62cbdf5b467c27fa1763b
pwang-610 <rewind1224@gmail.com>
2024-04-24 11:17:08 +0900

    update table data.json

---
+++ src/components/datatable/cmpTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import { Chart, registerables } from "chart.js";
+
+Chart.register(...registerables);
+
+  export default {
+    name: "cmpTable",
+    components: {
+      DataTable,
+    },
+    props: {
+      selectedMonth: {
+        type: String,
+        require: true, 
+      },
+    },
+
+
+  }
+</script>
+
+<style>
+
+</style>
